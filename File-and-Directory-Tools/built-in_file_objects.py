05aed3e13533318c8876f275119c9e7c74f6572b
ningningliu <lnn35@hotmail.com>
2017-07-21 12:07:04 +0100

    read/readline/readlines

--- File-and-Directory-Tools/built-in_file_objects.py
+++ File-and-Directory-Tools/built-in_file_objects.py
@@ -17,10 +17,13 @@ file = open('data.txt','w')
 # writing
 file.write('Hello file word! \n')
 file.write('Bye file world ! \n')
-# alternatively use writelines
+"""
+# writelines()
 # it isn't commonly used but can be emulated with for loop or other iteration tools.
 # it convenient to save output in a list to be written later.
-# file.writelines(['Hello file world! \n','Bye file world. \n'])
+
+file.writelines(['Hello file world! \n','Bye file world. \n'])
+"""
 
 # closing
 file.close() # colsed on gc and exit too
@@ -45,7 +48,37 @@ with open('data.txt') as fin, open('results','w') as fout:
 
 #  Input Files #
 file = open('data.txt')
-lines = file.readlines() # open input file object
+lines = file.readlines() # open input file object : 'r' default
+for line in lines: # use file line iterator
+    print(line, end='') # have a '\n' at end
+
+# read
+# there is many ways to read input file
+"""
+file.read() : 
+    returns a string containing all the characters or bytes stored in a file
+    
+file.read(N):
+    returns a string containing next N characters or bytes from the file
+    
+file.readline():
+    reads through the next \n and return a line string
+    
+file.readlines():
+    reads the entire file and returns a list of line strings
+    
+REMARK:
+    read() and readlines() are not good thing to do if loading huge files.
+    read(N) and readline() are safer which only call part of the file.
+    
+"""
+
+# call
+file.seek(0) # go back to the start of the file
+file.readline()
+
+
+
 
 
 
