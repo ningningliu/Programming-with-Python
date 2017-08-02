6a8bbd37057411a15890af424b06785779ccf3d2
ningningliu <lnn35@hotmail.com>
2017-08-02 17:12:10 +0100

    mysql introduction

---
+++ Acessing-Database/mysql.py
@@ -0,0 +1,23 @@
+import pymysql
+#!/usr/bin/python3
+
+
+# Open database connection
+db = pymysql.connect("localhost","root","","Test" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("select * from People")
+
+# Fetch all row using fetchall() method.
+data = cursor.fetchall()
+for row in data :
+    print (row[0], row[1])
+
+# close the cursor object
+cursor.close ()
+
+# disconnect from server
+db.close()
